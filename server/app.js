2cbeca8f1feef26af20347b5e7a5f871de225ca0
Anurag Singh <134362386+AnuragSingh012@users.noreply.github.com>
2024-07-04 13:13:55 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://quizify-client.vercel.app",
     credentials: true,
   })
 );
