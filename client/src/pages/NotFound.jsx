c20fb6e8c000d825cd688ee53c58a2e7c88179b1
Anurag Singh <134362386+AnuragSingh012@users.noreply.github.com>
2024-07-05 14:40:38 +0530

    Update NotFound.jsx

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="h-screen w-full flex flex-col gap-3 justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <p className="font-bold text-8xl">404</p>
+        <p className="font-medium text-gray-600 text-xl">Page not found</p>
+      </div>
+      <Link to="/" className="font-medium bg-black text-white px-4 py-2 rounded-lg">
+        GO TO HOMEPAGE
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
